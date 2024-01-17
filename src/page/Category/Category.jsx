e67d87aab9e3fc2513f227f7411230b257955b33
Duongbaxinh <duongbaxinh2021@gmail.com>
2024-01-17 09:27:56 +0700

    update

--- src/page/Category/Category.jsx
+++ src/page/Category/Category.jsx
@@ -52,7 +52,7 @@ function Category(props) {
             <div className='Body'>
                 <div className='list' style={{}}>
                     {categories.map((item, index) => (
-                        <div onClick={() => handleCategory(item.type)}><p>{item.title}</p></div>
+                        <div key={index} onClick={() => handleCategory(item.type)}><p>{item.title}</p></div>
                     ))}
                 </div>
                 <div className='grid_container'>
